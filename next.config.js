f997bee6c1c41580fb9d7aaac48893240b5e3f1e
Imandro <imandro@github.com>
2026-06-03 11:32:32 -0600

    chore: trigger vercel redeploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPWA = require('next-pwa')({
     dest: 'public',
+    // PWA is only enabled in production (build time)
     disable: process.env.NODE_ENV === 'development',
     register: true,
     skipWaiting: true,
