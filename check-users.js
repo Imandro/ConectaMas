f75f4837bbc77dcd342e7dd5d5c91a109b2741fb
Imandro <imandro@github.com>
2026-06-10 10:32:28 -0600

    chore: remover scripts extra no pertenecientes al proyecto

--- check-users.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } });
-p.user.findMany({ select: { id: true, email: true, name: true, username: true } }).then(users => {
-  console.log('Users in Neon: ' + users.length);
-  users.forEach(u => console.log(' - ' + u.id + ' | email=' + u.email + ' | name=' + u.name + ' | username=' + u.username));
-  p.$disconnect();
-});
